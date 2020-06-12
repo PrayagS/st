5534501ad0f5417aa27da7579b42f0c084c1d999
PrayagS <prayag.s@ahduni.edu.in>
2020-06-12 23:40:32 +0530

    Key-bindings use alt instead of ctrl+shift and vice-versa

--- config.h
+++ config.h
@@ -203,12 +203,12 @@ static MouseShortcut mshortcuts[] = {
 
 MouseKey mkeys[] = {
 		/* button               mask            function        argument */
-		{Button4, TERMMOD, kscrollup, {.i = 1}},
-		{Button5, TERMMOD, kscrolldown, {.i = 1}},
+		// {Button4, TERMMOD, kscrollup, {.i = 1}},
+		// {Button5, TERMMOD, kscrolldown, {.i = 1}},
 		{Button4, MODKEY, kscrollup, {.i = 1}},
 		{Button5, MODKEY, kscrolldown, {.i = 1}},
-		{Button4, ControlMask, zoom, {.f = +1}},
-		{Button5, ControlMask, zoom, {.f = -1}},
+		{Button4, TERMMOD, zoom, {.f = +1}},
+		{Button5, TERMMOD, zoom, {.f = -1}},
 };
 
 static char *openurlcmd[] = {"/bin/sh", "-c",
@@ -229,19 +229,17 @@ static Shortcut shortcuts[] = {
 		{XK_ANY_MOD, XK_Print, printsel, {.i = 0}},
 		{TERMMOD, XK_Prior, zoom, {.f = +1}},
 		{TERMMOD, XK_Next, zoom, {.f = -1}},
-		{MODKEY, XK_Home, zoomreset, {.f = 0}},
+		{TERMMOD, XK_Home, zoomreset, {.f = 0}},
 		{ShiftMask, XK_Insert, clippaste, {.i = 0}},
-		{TERMMOD, XK_C, clipcopy, {.i = 0}},
-		{TERMMOD, XK_V, clippaste, {.i = 0}},
+		{MODKEY, XK_C, clipcopy, {.i = 0}},
+		{MODKEY, XK_V, clippaste, {.i = 0}},
 		{MODKEY, XK_p, selpaste, {.i = 0}},
 		{MODKEY, XK_Num_Lock, numlock, {.i = 0}},
-		{MODKEY, XK_Control_L, iso14755, {.i = 0}},
-		{ShiftMask, XK_Page_Up, kscrollup, {.i = -1}},
-		{ShiftMask, XK_Page_Down, kscrolldown, {.i = -1}},
+		// {MODKEY, XK_Control_L, iso14755, {.i = 0}},
 		{MODKEY, XK_Page_Up, kscrollup, {.i = -1}},
 		{MODKEY, XK_Page_Down, kscrolldown, {.i = -1}},
-		{TERMMOD, XK_K, kscrollup, {.i = 1}},
-		{TERMMOD, XK_J, kscrolldown, {.i = 1}},
+		{MODKEY, XK_k, kscrollup, {.i = 1}},
+		{MODKEY, XK_j, kscrolldown, {.i = 1}},
 		{MODKEY, XK_Up, kscrollup, {.i = 1}},
 		{MODKEY, XK_Down, kscrolldown, {.i = 1}},
 		{MODKEY, XK_u, kscrollup, {.i = -1}},
@@ -252,9 +250,9 @@ static Shortcut shortcuts[] = {
 		{TERMMOD, XK_J, zoom, {.f = -1}},
 		{TERMMOD, XK_U, zoom, {.f = +2}},
 		{TERMMOD, XK_D, zoom, {.f = -2}},
-		{TERMMOD, XK_L, externalpipe, {.v = openurlcmd}},
-		{TERMMOD, XK_Y, externalpipe, {.v = copyurlcmd}},
-		{TERMMOD, XK_O, externalpipe, {.v = copyoutput}},
+		{MODKEY, XK_l, externalpipe, {.v = openurlcmd}},
+		{MODKEY, XK_y, externalpipe, {.v = copyurlcmd}},
+		{MODKEY, XK_o, externalpipe, {.v = copyoutput}},
 };
 
 /*
